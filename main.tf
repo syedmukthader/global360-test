35ff3b1cdaf11b19185cf34f1201e75ba3f5e21c
Syed-Mukthader <76219785+syedmukthader@users.noreply.github.com>
2026-08-01 05:10:23 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,31 +5,31 @@ locals {
 module "network" {
   source = "./modules/network"
 
-  name_prefix          = local.name_prefix
-  vpc_cidr             = var.vpc_cidr
-  public_subnet_cidrs  = var.public_subnet_cidrs
+  name_prefix         = local.name_prefix
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidrs = var.public_subnet_cidrs
 }
 
 module "security" {
   source = "./modules/security"
 
-  name_prefix        = local.name_prefix
-  vpc_id             = module.network.vpc_id
-  allowed_http_cidr  = var.allowed_http_cidr
+  name_prefix       = local.name_prefix
+  vpc_id            = module.network.vpc_id
+  allowed_http_cidr = var.allowed_http_cidr
 }
 
 module "compute" {
   source = "./modules/compute"
 
-  name_prefix        = local.name_prefix
-  vpc_id             = module.network.vpc_id
-  public_subnet_ids  = module.network.public_subnet_ids
-  alb_sg_id          = module.security.alb_sg_id
-  instance_sg_id     = module.security.instance_sg_id
-  instance_type      = var.instance_type
-  min_size           = var.min_size
-  max_size           = var.max_size
-  desired_capacity   = var.desired_capacity
-  enable_container   = var.enable_container
-  container_image    = var.container_image
+  name_prefix       = local.name_prefix
+  vpc_id            = module.network.vpc_id
+  public_subnet_ids = module.network.public_subnet_ids
+  alb_sg_id         = module.security.alb_sg_id
+  instance_sg_id    = module.security.instance_sg_id
+  instance_type     = var.instance_type
+  min_size          = var.min_size
+  max_size          = var.max_size
+  desired_capacity  = var.desired_capacity
+  enable_container  = var.enable_container
+  container_image   = var.container_image
 }
